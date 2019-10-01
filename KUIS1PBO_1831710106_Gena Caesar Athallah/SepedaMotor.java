a76f0ae0ceea07675ae3e9927a0e9ae8a76b6a77
GenaCaesar <50857347+GenaCaesar@users.noreply.github.com>
2019-10-01 10:31:32 +0700

    Add files via upload

---
+++ KUIS1PBO_1831710106_Gena Caesar Athallah/SepedaMotor.java
@@ -0,0 +1,17 @@
+package Kuis1;
+
+public class SepedaMotor {
+    public String merekSepeda;
+    public Mesin merekMesin;
+    
+    
+    public void setMerek(String merekSepeda){
+        this.merekSepeda = merekSepeda;
+    }
+    
+    public String getMerek(){
+        return merekSepeda;
+    }
+    
+    
+}
